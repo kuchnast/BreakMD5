5f68b5594136d7fe7ee018aa04fad200200520f7
kuchnast <252957@student.pwr.edu.pl>
2021-12-05 17:13:49 +0100

    Add MD5_test program

--- MD5_test.c
+++ MD5_test.c
@@ -5,14 +5,22 @@
  int main()
  {
      EVP_MD_CTX *mdctx;
-     const EVP_MD *md;
      char mess[] = "Test";
      unsigned char md_value[EVP_MAX_MD_SIZE];
      unsigned int md_len, i;
 
-     md = EVP_get_digestbyname("MD5");
      mdctx = EVP_MD_CTX_new();
-     EVP_DigestInit_ex(mdctx, md, NULL);
+     EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
+     EVP_DigestUpdate(mdctx, mess, strlen(mess));
+     EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+     //EVP_MD_CTX_free(mdctx);
+
+     printf("Digest is: ");
+     for (i = 0; i < md_len; i++)
+         printf("%02x", md_value[i]);
+     printf("\n");
+
+     EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
      EVP_DigestUpdate(mdctx, mess, strlen(mess));
      EVP_DigestFinal_ex(mdctx, md_value, &md_len);
      EVP_MD_CTX_free(mdctx);
